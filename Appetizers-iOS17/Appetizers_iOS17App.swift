2d3f32fef3735c4a3e056783fd3a2b1624ec315a
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-10 21:10:41 +0530

    Initial Commit

---
+++ Appetizers-iOS17/Appetizers_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_iOS17App.swift
+//  Appetizers-iOS17
+//
+//  Created by Mayur Vaity on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
