41415184b6d86102b792f4be550b333b84342c40
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-15 17:11:59 +0530

    Implement basic UI for Account page.

--- Appetizers-iOS17/Screens/AccountView/AccountView.swift
+++ Appetizers-iOS17/Screens/AccountView/AccountView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("🤣 Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birthday",
+                               selection: $birthDate,
+                               displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                        
+                } header: {
+                    Text("Requests")
+                }
+                .tint(.brandPrimary1) //can put tint on entire section, it will b applicable to all the view with in that section
+
+                
+            }
+            .navigationTitle("🤣 Account")
+            
             
         }
     }
